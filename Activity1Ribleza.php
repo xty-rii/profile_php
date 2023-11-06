4e898aec413a465431250f75f6c6c627d1af8d5a
Ris Ribleza <142079669+xty-rii@users.noreply.github.com>
2023-11-06 16:19:05 +0800

    Add files via upload

---
+++ Activity1Ribleza.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <script src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+    <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+
+<?php
+$name = "Christy 'Ris' Nevado Ribleza";
+$yrsection = "1st Yr - SBIT-IB";
+$age = 18;
+$location = "Quezon City, Philippines " ;
+$prevschool = "Lagro High School " ;
+$email = "ribleza.christy.nevado@gmail.com" ;
+$likes = "  Cats, Genshin, Arts, Matcha green, and Dosii" ;
+?>
+
+<div class="box">
+  <img src="pfp.jpg" alt="" class="box-img">
+  <h1> <?php echo $name ?> </h1>
+    <h5> Year/Section: <?php echo $yrsection; ?> </h5>
+
+<p> Age: <?php echo $age ?></p>
+<p> Location: <?php echo $location; ?> </p>
+<p> Previous School: <?php echo $prevschool; ?> </p>
+<p> Email Address: <?php echo $email; ?> </p>
+<p> Likes: <?php echo $likes; ?> </p>
+<br><br>
+
+        <p1><i>"The young today</i> </p1>
+        <p1><i>shall outperform us tomorrow"</i> </p1>
+</body>
+</html>
